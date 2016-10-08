32efbb0efd4199909ce6891b731465ff0c419480
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2016-10-08 13:27:09 -0500

    usando libreria del floating action button

--- MaterialDesign/app/src/main/java/com/kmmx/materialdesign/MainActivity.java
+++ MaterialDesign/app/src/main/java/com/kmmx/materialdesign/MainActivity.java
@@ -5,10 +5,13 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.github.clans.fab.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     CollapsingToolbarLayout collapsingToolbar;
+    FloatingActionButton fabSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +19,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        fabSend = (FloatingActionButton) findViewById(R.id.fabSend);
         collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
         setSupportActionBar(toolbar);
 
         collapsingToolbar.setContentScrimColor(getResources().getColor(R.color.colorAccent));
+
+        fabSend.setMax(100);
+        fabSend.setIndeterminate(false);
+        fabSend.setProgress(70, true);
     }
 }
