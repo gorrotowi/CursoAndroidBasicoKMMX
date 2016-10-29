014fa3233d96595a6d4a624e4b2db6596be4d634
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2016-10-29 13:55:28 -0500

    ejemplo notificacion //TODO extras intent

--- LocalNotification/app/src/main/java/com/kmmx/localnotification/MainActivity.java
+++ LocalNotification/app/src/main/java/com/kmmx/localnotification/MainActivity.java
@@ -1,13 +1,86 @@
 package com.kmmx.localnotification;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    Button button;
+    int idnot = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.btnAlert);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (getIntent().getExtras() != null) {
+            Bundle bundle = getIntent().getExtras();
+            int id = bundle.getInt("idnot", 0);
+            Log.e(TAG, "onCreate: " + id);
+            notificationManager.cancel(id);
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bitmap bmicon = ((BitmapDrawable) getResources().getDrawable(R.mipmap.ic_launcher)).getBitmap();
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(bmicon)
+                        .setContentTitle("Notificacion de alerta")
+                        .setContentText("Contenido de la notificacin")
+                        .setTicker("Notificacion...");
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.putExtra("idnot", idnot);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                        Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
+                builder.setContentIntent(pendingIntent);
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.notify(idnot, builder.build());
+                idnot++;
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Bitmap bmicon = ((BitmapDrawable) getResources().getDrawable(R.mipmap.ic_launcher)).getBitmap();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(bmicon)
+                .setContentTitle("Notificacion de alerta")
+                .setContentText("Contenido de la notificacin")
+                .setTicker("Notificacion...");
+
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(2, builder.build());
+
     }
 }
